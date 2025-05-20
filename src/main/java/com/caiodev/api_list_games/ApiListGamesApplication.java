3fe524a3002cdfdc64d33bb42c125bffa849bea8
Caio Costa <caiosantos.costa02@gmail.com>
2025-05-20 15:59:06 -0300

    Subindo projeto para o github

---
+++ src/main/java/com/caiodev/api_list_games/ApiListGamesApplication.java
@@ -0,0 +1,13 @@
+package com.caiodev.api_list_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiListGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiListGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caiodev/api_list_games/ApiListGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiodev.api_list_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiListGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
