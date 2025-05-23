396bfa9f136d26c519e95c49d2ddae479c29a9fd
Caio Costa <caiosantos.costa02@gmail.com>
2025-05-23 18:04:41 -0300

    Adicionado o Respository e o Service do sistema

--- src/main/java/com/caiodev/api_list_games/repositories/GameRepository.java
+++ src/main/java/com/caiodev/api_list_games/repositories/GameRepository.java
@@ -1,4 +1,9 @@
 package com.caiodev.api_list_games.repositories;
 
-public interface GameRepository {
+import com.caiodev.api_list_games.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+
 }
--- src/main/java/com/caiodev/api_list_games/services/GameService.java
+++ src/main/java/com/caiodev/api_list_games/services/GameService.java
@@ -1,4 +1,7 @@
 package com.caiodev.api_list_games.services;
+ 
+import org.springframework.stereotype.Service;
 
+@Service
 public class GameService {
 }
